a09378f8e25dab17fcdda61e3da17e95eccc49c7
Alejandro Aspinwall <aaspinwall@gmail.com>
2020-01-15 16:41:29 -0500

    full screen allowed

--- src/App.js
+++ src/App.js
@@ -5,26 +5,12 @@ import "./App.css";
 function App() {
   return (
     <div className='App'>
-      {/* <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header> */}
       <iframe
         width='100%'
-        height='315'
+        height='100%'
         src='https://us-east-2.sumerian.aws/1aced70c42cb4fca874815156d76209e.scene'
         frameborder='0'
-        allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+        allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; allowfullscreen'
         allowfullscreen
       ></iframe>
     </div>
