e225ddc27ada36ee70c9c2fa804d1f9844e75429
Alejandro Aspinwall <aaspinwall@gmail.com>
2020-01-17 16:42:44 -0500

    big link

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from "react";
 import "./App.css";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className='App'>
-      <iframe
+    <Wrapper className='App'>
+      <a href='https://us-east-1.sumerian.aws/e72a46f648b44037a3212b12deabd7f8.scene'>
+        Click here to experience AR
+      </a>
+
+      {/* <iframe
         width='100%'
         height='100%'
-        /* src='https://us-east-2.sumerian.aws/1aced70c42cb4fca874815156d76209e.scene' */
         src='https://us-east-1.sumerian.aws/43c5562ec9e04269a9012c4e26df000d.scene'
         frameborder='0'
         allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; allowfullscreen'
         allowfullscreen
-      ></iframe>
-    </div>
+      ></iframe> */}
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default App;
