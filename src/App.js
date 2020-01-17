59a60240b46693fec7b06e369b9ecd4a2645cc10
Alejandro Aspinwall <aaspinwall@gmail.com>
2020-01-17 14:07:52 -0500

    kitty publish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -8,7 +7,8 @@ function App() {
       <iframe
         width='100%'
         height='100%'
-        src='https://us-east-2.sumerian.aws/1aced70c42cb4fca874815156d76209e.scene'
+        /* src='https://us-east-2.sumerian.aws/1aced70c42cb4fca874815156d76209e.scene' */
+        src='https://us-east-1.sumerian.aws/43c5562ec9e04269a9012c4e26df000d.scene'
         frameborder='0'
         allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; allowfullscreen'
         allowfullscreen
